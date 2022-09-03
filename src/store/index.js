97981773fdea69560a2155a744920dbabe696788
Essayey <lucky84231@gmail.com>
2022-09-03 22:28:10 +0300

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -17,20 +17,22 @@ const todoReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_TODO:
             return { todos: [...state.todos, { text: action.payload, checked: false }] }
+
         case EDIT_TODO:
             return {todos: state.todos.map((todo, index) => {
                 if (index === action.payload.index) return {text: action.payload.text, checked: todo.checked};
                 return todo;
             })}
+
         case DELETE_TODO:
-            console.log({todos: state.todos.filter((_, index) => index !== action.payload)})
             return {todos: state.todos.filter((_, index) => index !== action.payload)}
-            
+
         case CHECK_TODO:
             return {todos: state.todos.map((todo, index) => {
                 if (index === action.payload) return {text: todo.text, checked: !todo.checked};
                 return todo;
             })}
+            
         default:
             return state
     }
